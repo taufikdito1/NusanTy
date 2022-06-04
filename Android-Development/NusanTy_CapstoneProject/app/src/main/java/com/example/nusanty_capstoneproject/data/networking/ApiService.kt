0c57b4e28359204b053ecaef64d5f2460ea1b711
Taufik Dito Wijaya <taufikdito1@gmail.com>
2022-06-04 16:51:21 +0700

    1. Change API for login and register 2. Add logic for user logout

--- Android-Development/NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/data/networking/ApiService.kt
+++ Android-Development/NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/data/networking/ApiService.kt
@@ -5,11 +5,13 @@ import com.example.nusanty_capstoneproject.data.model.login.RegisterResponse
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
     @FormUrlEncoded
-    @POST("register")
+    @Headers("api-key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.xmkUK_GYfENkOYqZa1NnNTy5IwTX9bS0hPFEYYR7jlo")
+    @POST("auth-api/register")
     fun registerUser(
         @Field("name") name: String,
         @Field ("email") email: String,
@@ -17,7 +19,8 @@ interface ApiService {
     ) : Call<RegisterResponse>
 
     @FormUrlEncoded
-    @POST("login")
+    @Headers("api-key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.xmkUK_GYfENkOYqZa1NnNTy5IwTX9bS0hPFEYYR7jlo")
+    @POST("auth-api/login")
     fun loginUser(
         @Field("email") email: String,
         @Field("password")password: String
--- Android-Development/NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/ui/activity/main/ui/akun/AkunFragment.kt
+++ Android-Development/NusanTy_CapstoneProject/app/src/main/java/com/example/nusanty_capstoneproject/ui/activity/main/ui/akun/AkunFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nusanty_capstoneproject.ui.activity.main.ui.akun
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.nusanty_capstoneproject.databinding.FragmentAkunBinding
+import com.example.nusanty_capstoneproject.helper.UserPreference
+import com.example.nusanty_capstoneproject.ui.activity.login.LoginActivity
 
 class AkunFragment : Fragment() {
 
     private var _binding: FragmentAkunBinding? = null
 
     private val binding get() = _binding!!
+
+    private lateinit var userPreference: UserPreference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +36,24 @@ class AkunFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userPreference = UserPreference(requireActivity())
+
+        btnAction()
+    }
+
+    private fun btnAction(){
+        binding.btnLogout.setOnClickListener {
+            userPreference.logout()
+            requireActivity().run {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
     }
 
     override fun onDestroyView() {
